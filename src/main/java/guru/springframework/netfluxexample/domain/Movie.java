8d657c99353a6cbd1870a47fc30c9987474dea73
Stefan Busnita <stefanbusnita@yahoo.com>
2017-12-04 16:29:02 +0100

    Quick dummy domain and repo for the test.

--- src/main/java/guru/springframework/netfluxexample/domain/Movie.java
+++ src/main/java/guru/springframework/netfluxexample/domain/Movie.java
@@ -1,5 +1,21 @@
 package guru.springframework.netfluxexample.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
+
+
+    private String id;
+
+    @NonNull
+    private String title;
+
 }
